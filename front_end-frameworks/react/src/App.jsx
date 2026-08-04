081c048b434831f97a75f800107dd7a7d951d97b
Yugz29 <yannduzelier@gmail.com>
2026-08-04 10:58:19 +0200

    Project setup

---
+++ front_end-frameworks/react/src/App.jsx
@@ -0,0 +1,19 @@
+import { Sparkles } from "lucide-react";
+
+function App() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <Sparkles className="w-12 h-12 text-blue-500" />
+
+      <h1 className="text-4xl font-bold">
+        React + Vite
+      </h1>
+
+      <h2 className="text-lg text-gray-600">
+        Project setup.
+      </h2>
+    </main>
+  );
+}
+
+export default App;
