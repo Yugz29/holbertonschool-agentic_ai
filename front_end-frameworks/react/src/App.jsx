8c5f326d1226a1b348dfb8b3d264e7d45baafee1
Yugz29 <yannduzelier@gmail.com>
2026-08-10 15:22:10 +0200

    chore: update App.jsx imports after refactoring

--- front_end-frameworks/react/src/App.jsx
+++ front_end-frameworks/react/src/App.jsx
@@ -1,19 +1,23 @@
-import Header from "./components/Header";
-import Hero from "./sections/Hero";
+import Header from "./components/layout/Header";
+import Hero from "./components/sections/Hero";
 import About from "./sections/About";
 import Features from "./sections/Features";
 import Insights from "./sections/Insights";
 import Contact from "./sections/Contact";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <div className="bg-black">
       <Header />
-      <Hero />
-      <About />
-      <Features />
-      <Insights />
-      <Contact />
+      <main>
+        <Hero />
+        <About />
+        <Features />
+        <Insights />
+        <Contact />
+      </main>
+      <Footer />
     </div>
   );
 }
